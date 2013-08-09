f6c59a5cc965dbeba61dfdabb990704f05ed9f0a
Andres <pipechg@hotmail.com>
2013-08-09 09:38:54 -0500

    First commit

---
+++ src/main/java/co/edu/um/leg1/Juegos.java
@@ -0,0 +1,11 @@
+package co.edu.um.leg1;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: sala9
+ * Date: 9/08/13
+ * Time: 09:28 AM
+ * To change this template use File | Settings | File Templates.
+ */
+public class Juegos {
+}
